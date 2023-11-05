c5d306df9091177229f8c75c44fd6911c4153825
farankinstan <nik.sever05@mail.ru>
2023-11-05 19:40:13 +0300

    reg&OPB

---
+++ libraries/ssoba_instructions/Components/Operational Block.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ssoba_library.Components
+{
+    internal class Operational_Block
+    {
+        public string ViewData { get { return Convert.ToString(result, 2); } }
+        public int x
+        {
+            get; set;
+        }
+
+        public int y
+        {
+            get; set;
+        }
+
+        public int result = 0x00000000;
+
+        public void Run(int x, int y, bool is_add)
+        {
+            if (is_add)
+            {
+                result = x + y;
+            }
+            result = x - y;
+        }
+    }
+}
---
+++ libraries/ssoba_instructions/Components/Register.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ssoba_library.Components
+{
+    internal class Register
+    {
+        public string ViewData { get { return Convert.ToString(data, 2); } }
+        public int data{ get;set; }
+    }
+}
